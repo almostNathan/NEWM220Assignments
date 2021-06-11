e0c8120ba062aaff7ffe3ec0d52af234ebc9344c
almostNathan <76500170+almostNathan@users.noreply.github.com>
2021-06-11 00:05:42 -0400

    lab 7 complete

--- labs/lab7/js/distanceCalculator.js
+++ labs/lab7/js/distanceCalculator.js
@@ -1,16 +1,19 @@
 function calcDist(){
+    //get all the input elements
     let x1Element = document.getElementById("inputX1")
     let y1Element = document.getElementById("inputY1")
     let x2Element = document.getElementById("inputX2")
     let y2Element = document.getElementById("inputY2")
 
+    //take the differences of the X and Y values entered
     let xDiff = Number(x1Element.value) - Number(x2Element.value) 
     let yDiff = Number(y1Element.value) - Number(y2Element.value) 
+
+    //calculate distance (pythag theorem)
     let dist = Math.sqrt(xDiff*xDiff + yDiff*yDiff)
 
+    //post the result to the screen
     let result = document.getElementById("result")
-
-
     result.innerHTML = `The distance between those two points is: ${dist}`
     result.style.fontSize = "34px"
 }
--- labs/lab7/js/numberGuesser.js
+++ labs/lab7/js/numberGuesser.js
@@ -1,20 +1,31 @@
-let responseElement = document.getElementById("response")
+//get user input elements
 let userGuessElement = document.getElementById("userGuess")
 
+//get response area element
+let responseElement = document.getElementById("response")
+
+//generate random number
 let targetNumber = Math.floor(Math.random()*10 + 1)
 
+//when button clicked
 function compareGuess(){
+    //get users guess
     let userGuess = userGuessElement.value
 
+    //if they are right
     if (userGuess == targetNumber){
+        //tell them they are right and make new number
         responseElement.innerHTML = "You guessed correct.\nI will think of a new number."
         targetNumber = Math.floor(Math.random()*10 + 1)
+    //if they are too high tell them
     }else if(userGuess > targetNumber){
         responseElement.innerHTML = "You guessed too high."
+    //if they are too low tell them
     }else if(userGuess < targetNumber){
         responseElement.innerHTML = "You guessed too low."
     }
 
+    //regardless clear the input area
     userGuessElement.value = ""
 }
 
