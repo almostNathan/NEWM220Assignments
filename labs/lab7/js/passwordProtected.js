868e1fe313e41d6b6c80c681c04a3502d5224c6f
almostNathan <76500170+almostNathan@users.noreply.github.com>
2021-06-11 01:00:33 -0400

    lab 7 complete

--- labs/lab7/js/passwordProtected.js
+++ labs/lab7/js/passwordProtected.js
@@ -1,15 +1,18 @@
-let container = document.getElementById("container")
-
 function checkLogin(){
+    //get user input elements
     let usernameElement = document.getElementById("username")
     let passwordElement = document.getElementById("password")
 
+    //get values for user input
     let username = usernameElement.value
     let password = passwordElement.value
 
+    //if username and password match
     if (username == "Username" && password == "Password"){
+        //tell them they logged in
         container.innerHTML = "Successfully Logged In"
     }else{
+        //call them a hacker
         container.innerHTML = "No Hackers Allowed"
     }
 
