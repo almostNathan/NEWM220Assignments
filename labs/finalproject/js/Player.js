697bc1b4c9deaa301b12b13034db3ee5e2d0c9bd
almostNathan <76500170+almostNathan@users.noreply.github.com>
2021-06-24 01:50:09 -0400

    final sumbission

--- labs/finalproject/js/Player.js
+++ labs/finalproject/js/Player.js
@@ -1,9 +1,14 @@
 class Player {
     constructor(){
         this.numOfBullets = 3;
+        this.heightFromMouse = 10
+        this.widthFromMouse = 20
+        this.center
     }
 
     draw(mousePos){
+
+        this.center = mousePos
         //make outline thicker and red colored
         strokeWeight(1)
         stroke("black")
@@ -18,4 +23,14 @@ class Player {
         ellipse(mousePos.x-20,mousePos.y-10, 20,10) 
     }
 
+    hasBeenHitBy(enemy){
+        
+        let enemyCorners = enemy.getCorners()
+
+        for (corner of enemyCorners){
+            if (corner.x > this.center.x-this.widthFromMouse && corner.x < this.center.x + this.widthFromMouse && corner.y > this.center.y - this.heightFromMouse && corner.y < this.center.y + this.heightFromMouse){
+
+            }
+        }
+    }
 }
